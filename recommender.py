bedb626db7baca028e356db6f76314ec3f989657
Alysson <alyssonggs@hotmail.com>
2018-06-25 13:09:40 -0300

    Limpeza de base

--- recommender.py
+++ recommender.py
@@ -91,20 +91,25 @@ y.shape
 
 data.head()
 
+#Não considerei descrição e usuário como um bom campo para treinamento
+data = data.drop('short desc', axis=1)
+data = data.drop('description', axis=1)
+data = data.drop('user', axis=1)
+#---------------------------------------------------------------------
 
 # ### Converts columns that are strings into numbers and store the labels in the labels array
 # ### the index of the labels array corresponds to the column number
 
 # In[14]:
-
-
-labels = [None] * 20
-for x in range(0, 20):
+labels = [None] * 17
+for x in range(0, 17):
     if type(data.iloc[:, x][0]) == str:
         data.iloc[:, x], labels[x] = pd.factorize(data.iloc[:, x])
     else:
         labels[x] = None
 
+
+print(labels)
 
 # In[15]:
 #Separa a coluna de item para ser o target
@@ -112,12 +117,9 @@ y = data['item']
 data = data.drop('item', axis=1)
 
 #Aplica os n vizinhos para treinar a base
-knn = KNeighborsClassifier(n_neighbors=3)
+knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(data.loc[:], y)
 
-
 ## COLUNAS
-#user cnt daytime weekday isweekend homework cost wheater country city category downloads developer language description name price rating shortdesc
-print(knn.predict([[5000, 0, 2, 1, 0, 5, 0, 1, 2, 1, 0, 3, 2, 1, 1, 0, 1, 0, 1]]))
-#print(data)
-
+#cnt daytime weekday isweekend homework cost wheater country city category downloads developer language name price rating
+print(knn.predict([[10, 1, 1, 0, 5, 0, 1, 2, 1, 0, 3, 2, 1, 0, 0, 22]]))
